5312dd772544d79ea434399a9063fa9611fb68a8
Imeldasiregar01 <146944594+Imeldasiregar01@users.noreply.github.com>
2024-03-10 20:49:03 +0700

    Add files via upload

---
+++ Loop.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "<h3>Looping I Love PHP</h3>";
+
+echo "LOOPING PERTAMA<br>";
+// Looping Pertama
+for ($i = 2; $i <= 20; $i += 2) {
+    echo "$i - I Love PHP<br>";
+}
+
+echo "<br>LOOPING KEDUA<br>";
+// Looping Kedua
+for ($i = 20; $i >= 2; $i -= 2) {
+    echo "$i - I Love PHP<br>";
+}
+
+?>
